17e345f371a727baf3ea7c40ae1bdad574a41fd8
bianca <bihehappy@gmail.com>
2024-06-19 12:49:41 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    //testando o pacote java
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!"); 
+        int A = 2;
+        int B = 3;
+        int soma = A + B;
+        System.out.println(soma);
+    }
+}
---
+++ src/anatomia3.java
@@ -0,0 +1,29 @@
+public class anatomia3 {
+    // Anatomia da classe 3 - Declarando variáveis e métodos
+    
+    /*estrtura da variável java:
+    Tipo NomeBemDefinido = Atribuição (opcional em alguns casos)
+    int idade = 23; 
+     */
+    String meuNome = "Bianca";
+    boolean sim = true; 
+    boolean nao = false;
+
+    String primeiroNome = "Vitor"; //variável
+    String segundoNome = "Ailton";
+    //String nomeCompleto = nomeCompleto(primeiroNome, segundoNome); //método
+
+
+    /*estrutura do método
+     TipoRetorno NomeObjetivoNoInfinitivo Parametro(s)
+     int somar (int numeroUm, int numero2)   (os parametros podiam ter tipos diferentes)
+     String formarCep (long cep)
+     */
+    //public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        //return "Resultado do método " + primeiroNome.concat("").concat(segundoNome);
+        //concatena primeiro e segundo nome
+        //duas formas de concatenar
+    //}
+    //defino uma classe public static, defino o tipo que ela vai retornar (String) junto com o nome da classe e os parametros
+    
+}
